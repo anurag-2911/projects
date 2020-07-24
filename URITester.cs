a5e566164aebebdefc4f4bf2f4f06190f7fd1fb8
Anurag Kumar <anurag.kumar@microfocus.com>
2020-07-24 23:13:39 +0530

    URITester

---
+++ URITester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class URITester
+    {
+        public void TestURI()
+        {
+            string path = @"\\svmzedc\shr_dat$\REF\PDF_XChange_Pro\_Install\7.0";
+            string ppath = Path.GetDirectoryName(path);
+            Console.WriteLine(ppath);
+        }
+    }
+}
