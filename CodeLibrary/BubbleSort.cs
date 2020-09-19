64668bec0070501e479da723d04a26dae22eaae5
Anurag Kumar <anurag.kumar@microfocus.com>
2020-09-19 20:39:41 +0530

    BubbleSort

--- CodeLibrary/BubbleSort.cs
+++ CodeLibrary/BubbleSort.cs
@@ -12,13 +12,17 @@ namespace CodeLibrary
         {
             int length = array.Length;
             for (int i = 0; i < length - 1; i++)
+            {
                 for (int j = 0; j < length - i - 1; j++)
+                {
                     if (array[j] > array[j + 1])
                     {
                         int temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                     }
+                }
+            }
         }
     }
 }
