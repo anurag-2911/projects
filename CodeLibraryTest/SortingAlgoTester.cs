9b8fc4eb1fb7387f665f8f8608f803bd952b3915
Anurag Kumar <anurag.kumar@microfocus.com>
2020-09-19 20:29:50 +0530

    CodeLibraryTest/SortingAlgoTester.cs

--- CodeLibraryTest/SortingAlgoTester.cs
+++ CodeLibraryTest/SortingAlgoTester.cs
@@ -1,17 +1,103 @@
 ﻿using CodeLibrary;
 using NUnit.Framework;
+using System;
 
 namespace CodeLibraryTest
 {
     [TestFixture]
     public class SortingAlgoTester
     {
-        Sorter sorter = new Sorter();
+        SortingAlgo sort = null;
         [TestFixtureSetUp]
         public void ClassSetup()
         {
 
         }
+        [SetUp]
+        public void MethodSetup()
+        {
 
+        }
+
+        [TestFixtureTearDown]
+        public void CleanUp()
+        {
+
+        }
+
+        [Test]
+        public virtual void TestBubbleSort()
+        {
+
+            // Arrange
+
+            int[] array = { 23, 4, 56, 1 };
+
+            int[] bubbleSortData = new int[array.Length];
+            int[] insertionSortData = new int[array.Length];
+            int[] mergeSortData = new int[array.Length];
+            int[] copyArray = new int[array.Length];
+
+            
+
+            Array.Copy(array, copyArray, array.Length);
+            Array.Copy(array, bubbleSortData, array.Length);
+            Array.Copy(array, insertionSortData, array.Length);
+            Array.Copy(array, mergeSortData, array.Length);
+
+
+
+
+            //Act
+
+            Array.Sort(copyArray);
+
+
+            sort = new BubbleSort();
+            sort.Sort(bubbleSortData);
+
+            sort = new InsertionSort();
+            sort.Sort(insertionSortData);
+
+            sort = new MergeSort();
+            sort.Sort(mergeSortData);
+
+
+            //Assert
+
+            bool areEqual;
+            areEqual = AreTwoArraysEqual(bubbleSortData, copyArray);
+
+            Assert.AreEqual(true, areEqual);
+
+           
+
+            areEqual = AreTwoArraysEqual(insertionSortData, copyArray);
+
+            Assert.AreEqual(true, areEqual);
+
+            
+            areEqual = AreTwoArraysEqual(mergeSortData, copyArray);
+
+            Assert.AreEqual(true, areEqual);
+
+
+
+        }
+
+        private static bool AreTwoArraysEqual(int[] array, int[] copyArray)
+        {
+            bool areEqual = true;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != copyArray[i])
+                {
+                    areEqual = false;
+                }
+            }
+
+            return areEqual;
+        }
     }
 }
