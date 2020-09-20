801fc0a6e0a635bfa2a45a78ebd2baf185a3954a
Anurag Kumar <anurag.kumar@microfocus.com>
2020-09-20 15:06:10 +0530

    basic algo

---
+++ CodeLibrary/SortingAlgo/SortingAlgo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary
+{
+    public abstract class SortingAlgo
+    {
+        public abstract void Sort(int[] array);
+        
+    }
+}
