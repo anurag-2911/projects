50b8830cffece79fd3af1809849f6a16c54138fd
Anurag Kumar <anurag.kumar@microfocus.com>
2020-09-20 15:03:44 +0530

    tests

---
+++ CodeLibraryTest/WorkTest.cs
@@ -0,0 +1,46 @@
+﻿using CodeLibrary;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibraryTest
+{
+    [TestFixture]
+    public class WorkTest
+    {
+        TimeScheduler timeScheduler;
+        [Test]
+        public virtual void TestMorningTime()
+        {
+            IDateTimeProvider dateTimeProvider = new MorningTime();
+            timeScheduler = new TimeScheduler(dateTimeProvider);
+            bool shallIwork=timeScheduler.ShallIWork01();
+            Assert.AreEqual(true, shallIwork);
+
+            shallIwork = (new TimeScheduler(new EveningTime())).ShallIWork01();
+
+            Assert.AreEqual(false, shallIwork);
+
+        }
+            
+
+    }
+
+    public class MorningTime : IDateTimeProvider
+    {
+        public DateTime GetDateTime()
+        {
+            return new DateTime(2020, 09, 12, 11, 0, 0);
+        }
+    }
+    public class EveningTime : IDateTimeProvider
+    {
+        public DateTime GetDateTime()
+        {
+            return new DateTime(2020, 09, 19, 11, 0, 0);
+        }
+    }
+}
